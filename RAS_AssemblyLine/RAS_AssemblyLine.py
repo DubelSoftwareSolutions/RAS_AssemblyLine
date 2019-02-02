4e2e7b5572ebc0ce5401557d35dd190d8c6c3473
Krzysztof Dąbek <k.narg.dabek@gmail.com>
2019-02-02 13:24:55 +0100

    More advanced, simplified example

--- RAS_AssemblyLine/RAS_AssemblyLine.py
+++ RAS_AssemblyLine/RAS_AssemblyLine.py
@@ -2,14 +2,25 @@ import snakes.plugins
 snakes.plugins.load('gv', 'snakes.nets', 'nets')
 from nets import *
 n = PetriNet('Tutorial Net')
-n.add_place(Place('p0', [1]))
+n.add_place(Place('p0', [1,1,1]))
 n.add_place(Place('p1', []))
-n.add_transition(Transition('t0',Expression('x>0')))
-n.add_input('p0','t0',Variable('x'))
-n.add_output('p1','t0',Variable('x'))
+
+n.add_transition(Transition('t0'))
+n.add_input('p0','t0',Value(1))
+n.add_output('p1','t0',Value(1))
+
+n.add_transition(Transition('t1'))
+n.add_input('p1','t1',Value(1))
+n.add_output('p0','t1',Value(1))
+
 
 n.draw('TutorialNet.png')
 print(n.transition('t0').modes())
-n.transition('t0').fire(Substitution(x=1))
-print(n.place('p0').tokens)
+n.transition('t0').fire(Substitution())
 n.draw('TutorialNetfire1.png')
+n.transition('t0').fire(Substitution())
+n.draw('TutorialNetfire2.png')
+n.transition('t1').fire(Substitution())
+n.draw('TutorialNetfire3.png')
+n.transition('t0').fire(Substitution())
+n.draw('TutorialNetfire4.png')
