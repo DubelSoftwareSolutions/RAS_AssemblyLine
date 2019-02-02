bc8ef3dd02ee4e6866e9b0ce0a9018d1159dcc31
Krzysztof Dąbek <k.narg.dabek@gmail.com>
2019-02-02 16:07:03 +0100

    building PT model embedded in a function

--- RAS_AssemblyLine/MultiProcessLineParametrised.py
+++ RAS_AssemblyLine/MultiProcessLineParametrised.py
@@ -3,47 +3,54 @@ snakes.plugins.load('gv', 'snakes.nets', 'nets')
 from nets import *
 net = PetriNet('Tutorial Net')
 
-numberOfMachines = 1
-numberOfRobots = 1
-numberOfProcesses = 2
-ProcessOperations = [[0], [0]]
+def buildLinePTModel(BufferCapacity, ProcessOperations):
+    numberOfRobots = 1
+    numberOfMachines = len(BufferCapacity)
+    numberOfProcesses = len(ProcessOperations)
+    for i in range(0, numberOfRobots):
+        net.add_place(Place('Robot'+str(i), [1]))
+    for i in range(0, numberOfMachines):
+        bufferMarking = [1] * BufferCapacity[i]
+        net.add_place(Place('Machine'+str(i),[1]))
+        net.add_place(Place('Buffer'+str(i),bufferMarking))
+    
+    
+    for i in range(0, len(ProcessOperations)):
+        for j in range(0, len(ProcessOperations[i])):
+            net.add_place(Place('P'+str(i)+'_O'+str(j)+'_Ready',[]))
+            net.add_place(Place('P'+str(i)+'_O'+str(j)+'_InProgress',[]))
+            net.add_place(Place('P'+str(i)+'_O'+str(j)+'_Finished',[]))
+    
+            net.add_transition(Transition('P'+str(i)+'_O'+str(j)+'_Prepare'))
+            net.add_input('Buffer'+str(ProcessOperations[i][j]),'P'+str(i)+'_O'+str(j)+'_Prepare',Value(1))
+            net.add_input('Robot0','P'+str(i)+'_O'+str(j)+'_Prepare',Value(1))
+            net.add_output('P'+str(i)+'_O'+str(j)+'_Ready','P'+str(i)+'_O'+str(j)+'_Prepare',Value(1))
+            net.add_output('Robot0','P'+str(i)+'_O'+str(j)+'_Prepare',Value(1))
+            if(j > 0):
+                net.add_input('P'+str(i)+'_O'+str(j-1)+'_Finished','P'+str(i)+'_O'+str(j)+'_Prepare',Value(1))
+                net.add_output('Buffer'+str(ProcessOperations[i][j-1]),'P'+str(i)+'_O'+str(j)+'_Prepare',Value(1))
+    
+            net.add_transition(Transition('P'+str(i)+'_O'+str(j)+'_Begin'))
+            net.add_input('P'+str(i)+'_O'+str(j)+'_Ready','P'+str(i)+'_O'+str(j)+'_Begin',Value(1))
+            net.add_input('Machine'+str(ProcessOperations[i][j]),'P'+str(i)+'_O'+str(j)+'_Begin',Value(1))
+            net.add_output('P'+str(i)+'_O'+str(j)+'_InProgress','P'+str(i)+'_O'+str(j)+'_Begin',Value(1))
+    
+            net.add_transition(Transition('P'+str(i)+'_O'+str(j)+'_Finish'))
+            net.add_input('P'+str(i)+'_O'+str(j)+'_InProgress','P'+str(i)+'_O'+str(j)+'_Finish',Value(1))
+            net.add_output('Machine'+str(ProcessOperations[i][j]),'P'+str(i)+'_O'+str(j)+'_Finish',Value(1))
+            net.add_output('P'+str(i)+'_O'+str(j)+'_Finished','P'+str(i)+'_O'+str(j)+'_Finish',Value(1))
 
-for i in range(0, numberOfRobots):
-    net.add_place(Place('Robot'+str(i), [1]))
-for i in range(0, numberOfMachines):
-    net.add_place(Place('Machine'+str(i),[1]))
-    net.add_place(Place('Buffer'+str(i),[1,1]))
+        net.add_transition(Transition('P'+str(i)+'_Finish'))
+        net.add_input('P'+str(i)+'_O'+str(len(ProcessOperations[i])-1)+'_Finished','P'+str(i)+'_Finish',Value(1))
+        net.add_input('Robot0','P'+str(i)+'_Finish',Value(1))
+        net.add_output('Buffer'+str(ProcessOperations[i][-1]),'P'+str(i)+'_Finish',Value(1))
+        net.add_output('Robot0','P'+str(i)+'_Finish',Value(1))
 
+    return net
 
-for i in range(0, len(ProcessOperations)):
-    for j in range(0, len(ProcessOperations[i])):
-        net.add_place(Place('P'+str(i)+'_O'+str(j)+'_Ready',[]))
-        net.add_place(Place('P'+str(i)+'_O'+str(j)+'_InProgress',[]))
-        net.add_place(Place('P'+str(i)+'_O'+str(j)+'_Finished',[]))
-
-        net.add_transition(Transition('P'+str(i)+'_O'+str(j)+'_Prepare'))
-        net.add_input('Buffer'+str(ProcessOperations[i][j]),'P'+str(i)+'_O'+str(j)+'_Prepare',Value(1))
-        net.add_input('Robot0','P'+str(i)+'_O'+str(j)+'_Prepare',Value(1))
-        net.add_output('P'+str(i)+'_O'+str(j)+'_Ready','P'+str(i)+'_O'+str(j)+'_Prepare',Value(1))
-        net.add_output('Robot0','P'+str(i)+'_O'+str(j)+'_Prepare',Value(1))
-        if(j > 0):
-            net.add_input('P'+str(i)+'_O'+str(j-1)+'_Finished','P'+str(i)+'_O'+str(j)+'_Prepare',Value(1))
-            net.add_output('Buffer'+str(ProcessOperations[i][j-1]),'P'+str(i)+'_O'+str(j)+'_Prepare',Value(1))
-
-        net.add_transition(Transition('P'+str(i)+'_O'+str(j)+'_Begin'))
-        net.add_input('P'+str(i)+'_O'+str(j)+'_Ready','P'+str(i)+'_O'+str(j)+'_Begin',Value(1))
-        net.add_input('Machine'+str(ProcessOperations[i][j]),'P'+str(i)+'_O'+str(j)+'_Begin',Value(1))
-        net.add_output('P'+str(i)+'_O'+str(j)+'_InProgress','P'+str(i)+'_O'+str(j)+'_Begin',Value(1))
-
-        net.add_transition(Transition('P'+str(i)+'_O'+str(j)+'_Finish'))
-        net.add_input('P'+str(i)+'_O'+str(j)+'_InProgress','P'+str(i)+'_O'+str(j)+'_Finish',Value(1))
-        net.add_output('Machine'+str(ProcessOperations[i][j]),'P'+str(i)+'_O'+str(j)+'_Finish',Value(1))
-        net.add_output('P'+str(i)+'_O'+str(j)+'_Finished','P'+str(i)+'_O'+str(j)+'_Finish',Value(1))
-
-    net.add_transition(Transition('P'+str(i)+'_Finish'))
-    net.add_input('P'+str(i)+'_O'+str(len(ProcessOperations[i])-1)+'_Finished','P'+str(i)+'_Finish',Value(1))
-    net.add_input('Robot0','P'+str(i)+'_Finish',Value(1))
-    net.add_output('Buffer'+str(ProcessOperations[i][-1]),'P'+str(i)+'_Finish',Value(1))
-    net.add_output('Robot0','P'+str(i)+'_Finish',Value(1))
-
-net.draw('OneProcessLineParametrised.png')
+if __name__ == "__main__":
+    BufferCapacity = [2,1]
+    ProcessOperations = [[0, 1]]
+    PTnet = buildLinePTModel(BufferCapacity,ProcessOperations)
+    print(PTnet.get_marking())
+    PTnet.draw('OneProcessLineParametrised.png')
