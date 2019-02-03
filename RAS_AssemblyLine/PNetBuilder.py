b7b01a39a07000aa2dc66d19b4d820d0e591b665
Dymitr Choroszczak <dymitr.choroszczak@gmail.com>
2019-02-03 11:18:11 +0100

    refactor PNetBuilder: global instances

--- RAS_AssemblyLine/PNetBuilder.py
+++ RAS_AssemblyLine/PNetBuilder.py
@@ -2,37 +2,45 @@ import yaml
 from PT_Simulator import *
 
 class PNetBuilder:
+    MachinesBufferCapacities = []
+    OperationOrders = []
+    NumberOfParts = []
+
     @classmethod
     def build(cls, fname):
         LineSimulator = PT_Simulator()
         with open(fname) as f:
             configuration = yaml.load(f)
 
-        MachinesBufferCapacities = cls.loadMachines(cls, configuration)
-        orders = cls.loadProcessOrders(cls, configuration)
+        cls.loadMachines(configuration)
+        cls.loadProcesses(configuration)
 
-        print(MachinesBufferCapacities)
-        print(orders)
+        print(cls.MachinesBufferCapacities)
+        print(cls.OperationOrders)
+        print(cls.NumberOfParts)
 
-    def loadMachines(self, configuration):
-        MachinesBufferCapacities = []
+    @classmethod
+    def loadMachines(cls, configuration):
+        cls.MachinesBufferCapacities = []
         Machines = configuration['machines']
 
         for machine in Machines:
-            MachinesBufferCapacities.append(Machines[machine]['capacity'])
+            cls.MachinesBufferCapacities.append(Machines[machine]['capacity'])
 
-        return MachinesBufferCapacities
+        return cls.MachinesBufferCapacities
 
-    def loadProcessOrders(self, configuration):
-        OperationOrders = []
-
+    @classmethod
+    def loadProcesses(cls, configuration):
+        cls.OperationOrders = []
+        cls.NumberOfParts = []
         Processes = configuration['processes']
 
         for process in Processes:
             order = []
+            cls.NumberOfParts.append(Processes[process]['number_of_parts'])
             for operation in Processes[process]['order']:
                 order.append(operation)
 
-            OperationOrders.append(order)
+            cls.OperationOrders.append(order)
 
-        return OperationOrders
+
